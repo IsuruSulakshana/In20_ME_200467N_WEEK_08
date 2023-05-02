755f6bae76ae19eb640a22cd9c05f07ba28e18a8
Isuru Sulakshana <pererawhis.20@uom.lk>
2023-05-02 16:45:09 +0530

    updated

---
+++ heap.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+// function to heapify the tree
+void heapify(int arr[], int n, int root)
+{
+    int largest = root;  
+    int left = 2 * root + 1; 
+    int right = 2 * root + 2;  
+
+
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != root) {
+        swap(arr[root], arr[largest]);
+
+        heapify(arr, n, largest);
+    }
+}
+
+// implementing heap sort
+void heapSort(int arr[], int n)
+{
+    // Build heap 
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    // Extracting elements from heap one by one
+    for (int i = n - 1; i > 0; i--) {
+        // Move current root to end
+        swap(arr[0], arr[i]);
+
+        // call max heapify on the reduced heap
+        heapify(arr, i, 0);
+    }
+}
+
+// print contents of array
+void displayArray(int arr[], int n)
+{
+    for (int i = 0; i < n; ++i)
+        cout << arr[i] << " ";
+    cout << "\n";
+}
+
+// main program
+int main()
+{
+    int heap_arr[] = { 5, 9, 3, 2, 7 };
+    int n = sizeof(heap_arr) / sizeof(heap_arr[0]);
+    cout << "Input array" << endl;
+    displayArray(heap_arr, n);
+
+    heapSort(heap_arr, n);
+
+    cout << "Sorted array" << endl;
+    displayArray(heap_arr, n);
+
+    return 0;
+}
